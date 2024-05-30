d890dddd96221ae308bd7158a2f5a68909ac1db6
Joseph Wilde <jwilde19@gmail.com>
2024-05-30 19:46:58 -0400

    Add user data

---
+++ src/data/userData.js
@@ -0,0 +1,12 @@
+export const userData = [
+    {
+        name: 'Silky Milky',
+        'icon-path': '@/public/silkymilky_icon.jpg',
+        tax: '',
+    }
+    {
+        name: 'OilMoney3000',
+        'icon-path': '@/public/oilmoney3000_icon.jpg',
+        tax: '',
+    }
+]
